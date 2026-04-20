bcb9a8d26e08ce2828bb793a59a8d1cf1a907317
Lasereyepop <ziyangzhou2003@gmail.com>
2026-04-19 17:03:53 -0700

    Made additional fixes to pipe.c

--- lab1/pipe.c
+++ lab1/pipe.c
@@ -12,50 +12,65 @@ int main(int argc, char *argv[]) {
   }
 
   int in_fd = STDIN_FILENO;
+  int pipe_fds[2] = {-1, -1};
   pid_t pids[argc];
+  int spawned_children = 0;
 
   for (int i = 1; i < argc; i++) {
-    int pipe_fds[2];
     int is_last = (i == argc - 1);
 
     if (!is_last) {
       if (pipe(pipe_fds) == -1)
-        exit(errno);
+        goto cleanup;
     }
 
     pid_t pid = fork();
     if (pid == -1)
-      exit(errno);
+      goto cleanup;
 
-    // Child
     if (pid == 0) {
       if (in_fd != STDIN_FILENO) {
         if (dup2(in_fd, STDIN_FILENO) == -1)
           exit(errno);
-        close(in_fd);
+        if (close(in_fd) == -1)
+          exit(errno);
       }
 
       if (!is_last) {
         if (dup2(pipe_fds[1], STDOUT_FILENO) == -1)
           exit(errno);
-        close(pipe_fds[1]);
-        close(pipe_fds[0]);
+        if (close(pipe_fds[1]) == -1)
+          exit(errno);
+        if (close(pipe_fds[0]) == -1)
+          exit(errno);
       }
 
       execlp(argv[i], argv[i], (char *)NULL);
 
+      int exec_errno = errno;
       perror("Execution Failed");
-      errno = EINVAL;
-      exit(errno);
-    } else { // Parent
-      pids[i] = pid;
 
-      if (in_fd != STDIN_FILENO)
-        close(in_fd);
+      if (exec_errno == ENOENT || exec_errno == EACCES ||
+          exec_errno == ENOEXEC) {
+        exit(EINVAL);
+      }
+
+      exit(exec_errno);
+    } else {
+      pids[i] = pid;
+      spawned_children++;
+
+      if (in_fd != STDIN_FILENO) {
+        if (close(in_fd) == -1)
+          goto cleanup;
+      }
 
       if (!is_last) {
-        close(pipe_fds[1]);
+        if (close(pipe_fds[1]) == -1)
+          goto cleanup;
         in_fd = pipe_fds[0];
+        pipe_fds[0] = -1;
+        pipe_fds[1] = -1;
       }
     }
   }
@@ -64,11 +79,44 @@ int main(int argc, char *argv[]) {
 
   for (int i = 1; i < argc; i++) {
     int status;
-    waitpid(pids[i], &status, 0);
+    if (waitpid(pids[i], &status, 0) == -1) {
+      int saved_errno = errno;
+      for (int j = i + 1; j < argc; j++) {
+        if (waitpid(pids[j], NULL, 0) == -1) {
+          perror("waitpid");
+        }
+      }
+      exit(saved_errno);
+    }
 
-    if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+    if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
       final_status = WEXITSTATUS(status);
+    }
   }
 
   return final_status;
+
+cleanup: {
+  int saved_errno = errno;
+
+  if (in_fd != STDIN_FILENO) {
+    if (close(in_fd) == -1)
+      perror("close");
+  }
+  if (pipe_fds[0] != -1) {
+    if (close(pipe_fds[0]) == -1)
+      perror("close");
+  }
+  if (pipe_fds[1] != -1) {
+    if (close(pipe_fds[1]) == -1)
+      perror("close");
+  }
+
+  for (int j = 1; j <= spawned_children; j++) {
+    if (waitpid(pids[j], NULL, 0) == -1)
+      perror("waitpid");
+  }
+
+  exit(saved_errno);
+}
 }
