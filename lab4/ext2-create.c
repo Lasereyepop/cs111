ff18f2318c282f97958ab67e45aa7f58eb2cc4b4
Lasereyepop <ziyangzhou2003@gmail.com>
2026-06-01 20:37:57 -0700

    indexing error fix v2

--- lab4/ext2-create.c
+++ lab4/ext2-create.c
@@ -289,7 +289,7 @@ void write_block_bitmap(int fd) {
   for (int i = 0; i <= LAST_BLOCK; i++)
     map_value[i / 8] |= (1 << (i % 8));
 
-  for (int i = NUM_BLOCKS - 1; i < BLOCK_SIZE * 8; i++)
+  for (int i = NUM_BLOCKS; i < BLOCK_SIZE * 8; i++)
     map_value[i / 8] |= (1 << (i % 8));
 
   if (write(fd, map_value, BLOCK_SIZE) != BLOCK_SIZE) {
